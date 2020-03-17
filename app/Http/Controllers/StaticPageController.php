2e46dc5fc5085824c491c9d6a2e4f3e230643dbd
simona <palumbosimona118@gmail.com>
2020-03-17 19:38:39 +0100

    up view

--- app/Http/Controllers/StaticPageController.php
+++ app/Http/Controllers/StaticPageController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class StaticPageController extends Controller
 {
-    //
+    public function home()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
 }
--- app/Http/Controllers/studentController.php
+++ app/Http/Controllers/studentController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class studentController extends Controller
 {
-    //
+  public function home()
+  {
+
+  }
+
 }
